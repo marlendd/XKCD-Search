662663f005c3f78c25e6706eceeee820a9c3f678
marlendd <vadim150605@gmail.com>
2026-04-26 20:10:00 +0300

    feat(words): add word normalization service

---
+++ search-services/words/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+	wordspb "github.com/marlendd/XKCD-Search/proto/words"
+	"github.com/marlendd/XKCD-Search/words/words"
+)
+
+type Config struct {
+	GRPCPort string `yaml:"grpc_port" env:"WORDS_ADDRESS" env-default:"28082"`
+}
+
+type server struct {
+	wordspb.UnimplementedWordsServer
+}
+
+const maxPhraseLen = 20000
+
+func (s *server) Ping(_ context.Context, in *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, nil
+}
+
+func (s *server) Norm(_ context.Context, in *wordspb.WordsRequest) (*wordspb.WordsReply, error) {
+	if len(in.GetPhrase()) > maxPhraseLen {
+		return nil, status.Error(codes.ResourceExhausted, "message size exceeds 20000 bytes")
+	}
+	normalized := words.Norm(in.GetPhrase())
+	return &wordspb.WordsReply{Words: normalized}, nil
+}
+
+func main() {
+	cfgPath := flag.String("config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(*cfgPath, &cfg); err != nil {
+		if os.IsNotExist(err) {
+			if err := cleanenv.ReadEnv(&cfg); err != nil {
+				slog.Error("failed to read env", "error", err)
+				os.Exit(1)
+			}
+		} else {
+			slog.Error("failed to read config", "error", err)
+			os.Exit(1)
+		}
+	}
+
+	if err := run(cfg); err != nil {
+		slog.Error("server failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run(cfg Config) error {
+	listener, err := net.Listen("tcp", cfg.GRPCPort)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	wordspb.RegisterWordsServer(s, &server{})
+	reflection.Register(s)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		slog.Debug("shutting down server")
+		s.GracefulStop()
+	}()
+
+	if err := s.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
+
+	return nil
+}
